3914e68b296a71111c76fb93aef913b5040fa35b
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-06 17:16:30 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-
-  webpack: (config) => {
-    config.resolve.fallback = { 
-      ...config.resolve.fallback, 
-      fs: false,
-      net: false,
-      tls: false 
-    };
-
-    return config;
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'https',
-        hostname: 'decifrandocoracoes.com',
-      }
-    ]
-  }
-};
+}
 
 export default nextConfig;
