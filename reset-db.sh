b11491fecaca3ac9a50249aed86e5c459496a82c
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-06 17:58:12 -0300

    Update reset-db.sh

--- reset-db.sh
+++ reset-db.sh
@@ -33,6 +33,6 @@ print_box "Matando processos na porta 3000..."
 fuser -k 3000/tcp
 
 print_box "Limpando Logs..."
-tail -f /www/server/nodejs/vhost/logs/decifrandocoracoes.log
+echo "" > /www/server/nodejs/vhost/logs/decifrandocoracoes.log
 
 print_box "Processo concluído!"
