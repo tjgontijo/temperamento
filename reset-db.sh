d2cdc674dd3c309a3da97f8a86a0b47827ccd7ab
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-06 13:27:27 -0300

    Update reset-db.sh

--- reset-db.sh
+++ reset-db.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-echo "Removendo diretórios e arquivos desnecessários..."
+print_box() {
+    local message="$1"
+    local length=${#message}
+    local padding=2
+    local border_length=$((length + padding * 2))
+    
+    printf '┌%*s┐\n' "$border_length" | tr ' ' '-'
+    printf '│ %*s │\n' "$((length + padding))" "$message"
+    printf '└%*s┘\n' "$border_length" | tr ' ' '-'
+}
+
+print_box "Removendo diretórios e arquivos desnecessários..."
 rm -rf .next node_modules/@prisma/client node_modules/.prisma prisma/migrations
 
-echo "Limpando cache do npm..."
+print_box "Limpando cache do npm..."
 npm cache clean --force
 
-echo "Removendo cache do node_modules."
+print_box "Removendo cache do node_modules."
 rm -rf node_modules/.cache
 
-echo "Instalando dependências..."
+print_box "Resetando banco de dados..."
+npx prisma migrate reset --force
+
+print_box "Instalando dependências..."
 npm install
 
-echo "Executando migrações do Prisma..."
+print_box "Executando migrações do Prisma..."
 npx prisma migrate dev --name init --force-reset
 
-echo "Gerando cliente do Prisma..."
+print_box "Gerando cliente do Prisma..."
 npx prisma generate
 
-echo "Processo concluído!"
+print_box "Processo concluído!"
