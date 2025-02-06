ecab69805c549553979c3057bd4721a6f702569e
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-05 21:33:06 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      ...config.resolve.fallback, 
+      fs: false,
+      net: false,
+      tls: false 
+    };
+
+    return config;
+  },
+
+  images: {
+    domains: ['localhost', 'decifrandocoracoes.com']
+  }
 };
 
 export default nextConfig;
