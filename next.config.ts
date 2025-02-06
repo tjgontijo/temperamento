39c993426ad9d300e5d5f0cb16aab61e5c2e66fa
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-06 19:19:38 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-}
+  output: 'standalone', // Tente adicionar isso se não estiver usando
+  reactStrictMode: true,
+};
 
 export default nextConfig;
