294a47716e82adff57e02c65a9005fa73f918901
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-13 13:01:23 -0300

    Update HeadScripts.tsx

--- src/components/scripts/HeadScripts.tsx
+++ src/components/scripts/HeadScripts.tsx
@@ -1,29 +1,12 @@
 'use client';
 
 import Script from 'next/script';
-import { useEffect } from 'react';
-
-// Corrige o erro de TypeScript declarando a propriedade pixelId no window
-declare global {
-  interface Window {
-    pixelId?: string;
-  }
-}
 
 export function HeadScripts() {
-  useEffect(() => {
-    // Garante que o script do UTMify Pixel só seja inserido uma vez
-    if (!window.pixelId && !document.getElementById('utmify-pixel-script')) {
-      window.pixelId = "67ad2512def830eb4835837c";
-
-      const script = document.createElement("script");
-      script.id = "utmify-pixel-script";
-      script.async = true;
-      script.defer = true;
-      script.src = "https://cdn.utmify.com.br/scripts/pixel/pixel.js";
-      document.head.appendChild(script);
-    }
-  }, []);
+  // Não renderiza scripts em desenvolvimento
+  if (process.env.NODE_ENV === 'development') {
+    return null;
+  }
 
   return (
     <>
@@ -38,18 +21,38 @@ export function HeadScripts() {
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NW45QNJV');
-          `,
+          `
         }}
       />
       {/* End Google Tag Manager */}
 
-      {/* UTMify UTMs */}
-      <Script
-        id="utmify-utms"
+      {/* UTMify Pixel */}
+      {/* <Script
+        id="utmify-pixel"
         strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.pixelId = "67ad2512def830eb4835837c";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `
+        }}
+      /> */}
+      {/* End UTMify Pixel */}
+
+      {/* UTMify UTMs */}
+      {/* <Script
+        id="utmify-utms"
         src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+        strategy="afterInteractive"
+        async
+        defer
         data-utmify-prevent-subids
-      />
+        data-utmify-debug="false"
+      /> */}
       {/* End UTMify UTMs */}
     </>
   );
