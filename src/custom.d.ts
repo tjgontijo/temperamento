9819abf82d7e07fc0e0a86769992e2c6bda93a40
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-13 20:51:54 -0300

    Update custom.d.ts

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -9,5 +9,6 @@ declare global {
   interface Window {
     pixelId?: string | null;
     sha256?: ((input: string) => string) | undefined;
+    fbq?: (type: 'track' | 'trackCustom', eventName: string, data?: Record<string, string | number | undefined>) => void;
   }
 }
