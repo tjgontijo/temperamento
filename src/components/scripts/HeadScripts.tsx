95bda1d93e08e249fa93f0626e76192a441270ed
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-27 14:38:12 -0300

    Update HeadScripts.tsx

--- src/components/scripts/HeadScripts.tsx
+++ src/components/scripts/HeadScripts.tsx
@@ -6,17 +6,20 @@ import Script from 'next/script';
 export function HeadScripts() {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-11003501383"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-tag-manager" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'AW-11003501383');
+          // Verifica se o dataLayer já existe antes de inicializar
+          if (typeof window.dataLayer === 'undefined') {
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+          }
+          (function(w,d,s,l,i){
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s);
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NW45QNJV');
         `}
       </Script>
     </>
