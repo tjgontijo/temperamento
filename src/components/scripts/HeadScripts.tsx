eb04b85d358e9051c2cbf63133082012a4b1ac2e
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-27 14:48:30 -0300

    Update HeadScripts.tsx

--- src/components/scripts/HeadScripts.tsx
+++ src/components/scripts/HeadScripts.tsx
@@ -6,8 +6,9 @@ import Script from 'next/script';
 export function HeadScripts() {
   return (
     <>
+      {/* Google tag (gtag.js) */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-11003501383"
+        src="https://www.googletagmanager.com/gtag/js?id=G-HNWH7B4YMX"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -16,7 +17,8 @@ export function HeadScripts() {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'AW-11003501383');
+          gtag('config', 'G-HNWH7B4YMX');  // Google Analytics
+          gtag('config', 'AW-11003501383'); // Google Ads
         `}
       </Script>
     </>
