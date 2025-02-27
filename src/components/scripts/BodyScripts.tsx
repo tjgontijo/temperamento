d84bb96ff0292c710287cdd162f4e8669de51588
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-27 14:38:14 -0300

    Update BodyScripts.tsx

--- src/components/scripts/BodyScripts.tsx
+++ src/components/scripts/BodyScripts.tsx
@@ -3,6 +3,20 @@
 export function BodyScripts() {
   return (
     <>
+      {/* Google Tag Manager (noscript) */}
+      <noscript 
+        dangerouslySetInnerHTML={{
+          __html: `
+            <iframe 
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NW45QNJV"
+              height="0" 
+              width="0" 
+              style="display:none;visibility:hidden"
+            ></iframe>
+          `
+        }}
+      />
+      {/* End Google Tag Manager (noscript) */}
     </>
   );
 }
