ee98785723f6e3071e8f020409477a7010ede1ec
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-28 11:15:37 -0300

    Update HeadScripts.tsx

--- src/components/scripts/HeadScripts.tsx
+++ src/components/scripts/HeadScripts.tsx
@@ -1,9 +1,23 @@
 // src/components/HeadScripts.tsx
 'use client';
 
-export function HeadScripts() {
+import { FC } from 'react';
+
+export const HeadScripts: FC = () => {
   return (
     <>
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "q8wnma77xf");
+          `
+        }}
+      />
     </>
   );
-}
+};
