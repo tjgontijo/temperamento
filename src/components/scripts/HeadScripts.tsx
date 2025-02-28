755538735d88f63d5143c1ca2e8a259eacb31c65
Thiago Gontijo <tjgontijo@gmail.com>
2025-02-28 11:23:36 -0300

    Update HeadScripts.tsx

--- src/components/scripts/HeadScripts.tsx
+++ src/components/scripts/HeadScripts.tsx
@@ -1,23 +1,18 @@
 // src/components/HeadScripts.tsx
 'use client';
 
-import { FC } from 'react';
+import Script from 'next/script';
 
-export const HeadScripts: FC = () => {
+export default function HeadScripts() {
   return (
-    <>
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "q8wnma77xf");
-          `
-        }}
-      />
-    </>
+    <Script id="clarity" strategy="afterInteractive">
+      {`
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "q8wnma77xf");
+      `}
+    </Script>
   );
-};
+}
